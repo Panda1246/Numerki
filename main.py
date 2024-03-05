c865b6191dd820f931d9d547132dbee09e5568d3
Jakub Samek <247781@edu.p.lodz.pl>
2024-03-05 11:44:34 +0100

    proste menu done

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# wpisywanie wspolczynnikow
-lenght = int(input("Podaj najwyzsza potege wielomianu"))
-values = []
-
-for i in range(lenght + 1):
-    values.append(int(input("Podaj wspolczynnik do x^" + str(i))))
-
-# wpisanie x
-x = int(input("Podaj wartosc x"))
-
-
 def Horner_value(x, values, lenght):
     final_value = 0
     for i in range(lenght):
@@ -30,4 +19,28 @@ def bisekcja(a, b, wspolczynniki):
     return x
 
 
-print(Horner_value(x, values, len(values)))
+
+
+czy_dziala=True
+while czy_dziala:
+    jakaFunkcja = input("Jaka funkcje wybierasz? \n W - wielomian; \n T - trygonometryczna \n E - Wykładnicza \n Z - zlozona \n Q - wyjście")
+    if jakaFunkcja == 'Q' or jakaFunkcja=='q':
+        czy_dziala=False
+    else:
+        jakieOgraniczenie = float(input("\n Warunek konca"))
+        if jakaFunkcja == 'W' or jakaFunkcja=='w':
+            # wpisywanie wspolczynnikow
+            lenght = int(input("Podaj najwyzsza potege wielomianu"))
+            values = []
+
+            for i in range(lenght + 1):
+                values.append(int(input("Podaj wspolczynnik do x^" + str(i))))
+
+            # wpisanie x
+            x = int(input("Podaj wartosc x"))
+        if jakaFunkcja == 'T' or jakaFunkcja == 't':
+            pass
+        if jakaFunkcja == 'E' or jakaFunkcja=='e':
+            pass
+        if jakaFunkcja == 'Z' or jakaFunkcja=='z':
+            pass
