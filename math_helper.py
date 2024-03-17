6c3cda9b870fa0440d5ae8fe6a360ece6f0cbb5f
Jakub Samek <247781@edu.p.lodz.pl>
2024-03-17 13:09:19 +0100

    final

--- math_helper.py
+++ math_helper.py
@@ -6,7 +6,7 @@ def function_1(x):
 
 
 def function_2(x):
-    return 2 * math.sin(x) * x ** 2
+    return 2 * math.sin(x) * (x * x)
 
 
 def function_3(x):
@@ -33,21 +33,23 @@ def value(x, function):
     elif function == '5':
         return function_5(x)
 
+
 def bisection_iteration(a, b, function, iteration):
     x = (a + b) / 2.0
     i = 0
-    while  i < iteration:
+    while i < iteration:
         value_of_b = value(b, function)
         value_of_x = value(x, function)
-        if math.fabs(value_of_x) <= 0.000001:
-            return x, i
-        elif (value_of_b < 0.0 and value_of_x < 0.0) | (value_of_b > 0.0 and value_of_x > 0.0):
+       # if math.fabs(value_of_x) <= 0.000001:
+        #    return x, i
+        if (value_of_b < 0.0 and value_of_x < 0.0) | (value_of_b > 0.0 and value_of_x > 0.0):
             b = x
         else:
             a = x
         x = (a + b) / 2
         i += 1
     return x, i
+
 
 def bisection_accuracy(a, b, function, eps):
     x = (a + b) / 2.0
@@ -56,9 +58,8 @@ def bisection_accuracy(a, b, function, eps):
     while math.fabs(x - previous_x) >= eps:
         value_of_b = value(b, function)
         value_of_x = value(x, function)
-        if math.fabs(value_of_x) <= 0.000001:
-            return x, i
-        elif (value_of_b < 0.0 and value_of_x < 0.0) | (value_of_b > 0.0 and value_of_x > 0.0):
+
+        if (value_of_b < 0.0 and value_of_x < 0.0) | (value_of_b > 0.0 and value_of_x > 0.0):
             b = x
         else:
             a = x
@@ -69,7 +70,6 @@ def bisection_accuracy(a, b, function, eps):
 
 
 def secant_method_iteration(a, b, function, iteration):
-
     i = 0
     while True:
         value_of_a = value(a, function)
@@ -80,11 +80,12 @@ def secant_method_iteration(a, b, function, iteration):
             print("\nDivision by zero\n")
             return x, i
 
-        if math.fabs(value(x,function)) <=0.00001 and i < iteration:
+        if  i == iteration:
             return x, i
         b = a
         a = x
         i += 1
+
 
 def secant_method_accuracy(a, b, function, eps):
     value_of_a = value(a, function)
@@ -102,14 +103,15 @@ def secant_method_accuracy(a, b, function, eps):
         except:
             print("\nDivision by zero\n")
             return x, i
-        if math.fabs(x - previous_x) <= eps and math.fabs(value(x,function)):
+        if math.fabs(x - previous_x) <= eps:
             return x, i
         i += 1
 
 
 def choose_your_method(a, b, function, eps_or_iteration, method):
     if method == '1':
-        return bisection_iteration(a, b, function, eps_or_iteration),  secant_method_iteration(a, b, function, eps_or_iteration)
+        return bisection_iteration(a, b, function, eps_or_iteration), secant_method_iteration(a, b, function,
+                                                                                              eps_or_iteration)
     elif method == '2':
-        return bisection_accuracy(a, b, function, eps_or_iteration), secant_method_accuracy(a, b, function, eps_or_iteration)
-
+        return bisection_accuracy(a, b, function, eps_or_iteration), secant_method_accuracy(a, b, function,
+                                                                                            eps_or_iteration)
