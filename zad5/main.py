bd7049f02912f463bae702fb0b7cb5f4753302be
Jakub Samek <247781@edu.p.lodz.pl>
2024-05-27 17:16:26 +0200

    zad 5 - almost complete

---
+++ zad5/main.py
@@ -0,0 +1,46 @@
+import math_helper as mt
+import matplotlib.pyplot as plt
+import numpy as np
+
+czy_dziala = True
+while czy_dziala:
+
+    jakaFunkcja = input("Jaka funkcje wybierasz? \n "
+                        "1 - 8x + 3 \n "
+                        "2 - |x - 5| \n "
+                        "3 - 1 + x - 2x^2\n "
+                        "4 - cos(x) / x \n "
+                        "5 - sin(x) - 2cos(x-5) \n "
+                        "q - wyjście\n")
+    if jakaFunkcja == 'Q' or jakaFunkcja == 'q':
+        czy_dziala = False
+
+    else:
+        func = mt.function(jakaFunkcja)
+
+        a = float(input("Podaj początek przedziału aproksymacji: "))
+        b = float(input("Podaj koniec przedziału aproksymacji: "))
+        degree = int(input("Podaj stopień wielomianu aproksymacyjnego: "))
+        num_nodes = degree + 1
+
+        x = np.linspace(a, b, num_nodes)
+        y, dy = func(x)
+
+        z, coefficients = mt.hermite_interpolation_coefficients(x, y, dy)
+
+        x_dense = np.linspace(a, b, 500)
+        y_dense = np.array([mt.horner_scheme(coefficients, xi, z) for xi in x_dense])
+        y_true = func(x_dense)
+        error = np.abs(y_true - y_dense)
+        print(f"Średni błąd aproksymacji: {np.mean(error[0])}")
+        #print(f"Maksymalny błąd aproksymacji: {np.max(error)}")
+
+        plt.plot(x_dense, y_true[0], label="Funkcja oryginalna")
+        plt.plot(x_dense, y_dense, label="Aproksymacja Hermite'a")
+        # plt.scatter(x, y, color='red', zorder=5, label="Punkty interpolacyjne")
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title("Aproksymacja wielomianami Hermite'a")
+        plt.legend()
+        plt.show()
+        input()
---
+++ zad5/math_helper.py
@@ -0,0 +1,66 @@
+import math
+import numpy as np
+
+
+def Horner_value(x, values, lenght):
+    final_value = 0
+    for i in range(lenght):
+        final_value = final_value * x + values[i]
+    return final_value
+
+
+def function_1(x):
+    return (8 * x) + 3, np.ones_like(x) * 8
+
+def function_2(x):
+    return np.abs(x - 5), (x-5)/np.abs(x-5)
+
+
+def function_3(x):
+    return Horner_value(x, [-2, 1, 1], 3), Horner_value(x, [-4, 1], 2)
+
+
+def function_4(x):
+    return np.cos(x) / x, (-x*np.sin(x)-np.cos(x))/x**2
+
+
+def function_5(x):
+    return np.sin(x) - 2 * np.cos(x - 5), np.cos(x) + 2*np.sin(x-5)
+
+def horner_scheme(coefficients, x, z):
+    n = len(coefficients)
+    result = coefficients[-1]
+    for i in range(n - 2, -1, -1):
+        result = result * (x - z[i]) + coefficients[i]
+    return result
+def function( fun):
+    if fun == '1':
+        return function_1
+    elif fun == '2':
+        return function_2
+    elif fun == '3':
+        return function_3
+    elif fun == '4':
+        return function_4
+    elif fun == '5':
+        return function_5
+def hermite_interpolation_coefficients(x, y, dy):
+    n = len(x)
+    z = np.zeros(2 * n)
+    Q = np.zeros((2 * n, 2 * n))
+
+    for i in range(n):
+        z[2 * i] = z[2 * i + 1] = x[i]
+        Q[2 * i, 0] = Q[2 * i + 1, 0] = y[i]
+        Q[2 * i + 1, 1] = dy[i]
+
+        if i != 0:
+            Q[2 * i, 1] = (Q[2 * i, 0] - Q[2 * i - 1, 0]) / (z[2 * i] - z[2 * i - 1])
+
+    for i in range(2, 2 * n):
+        for j in range(2, i + 1):
+            Q[i, j] = (Q[i, j - 1] - Q[i - 1, j - 1]) / (z[i] - z[i - j])
+
+    return z, Q.diagonal()
+
+
